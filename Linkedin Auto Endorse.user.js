95c80ed56c6b33ef52850deb54cebd6e00e349e2
Ivankalachikov <ivan.kalachikov@gmail.com>
2023-09-18 14:38:46 +0800

    Add files via upload

---
+++ Linkedin Auto Endorse.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name Linkedin Auto Endorse
+// @namespace OrangeMonkey Scripts
+// @grant none
+// ==/UserScript==
+
+const endorseSkillsHandler = async (e, iteration = 0) => {
+  e.target.style.backgroundColor = '#aaaaaa';
+  e.target.setAttribute('disabled', 'disabled');
+  const endorseButtons = document
+  	.querySelectorAll('.artdeco-tabpanel:not(.artdeco-tabpanel--hidden) .pvs-list__paged-list-item button.artdeco-button:not(:has(.artdeco-button__icon))');
+  if (endorseButtons.length < 1) {
+    e.target.removeAttribute('disabled');
+    e.target.style.backgroundColor = '#33b249';
+    return;
+  }
+  const pressEndorseButtonsPromises = [...endorseButtons].map((button, i) => new Promise((resolve) => {
+    setTimeout(() => {
+      button.scrollIntoView({ behavior: 'smooth', block: 'center' });;
+      button.click()
+      resolve();
+    }, i * 500);
+  }))
+  await Promise.allSettled(pressEndorseButtonsPromises);
+  const restEndorseButtons = document
+  	.querySelectorAll('.artdeco-tabpanel:not(.artdeco-tabpanel--hidden) .pvs-list__paged-list-item .artdeco-button:not(:has(.artdeco-button__icon))');
+  if (restEndorseButtons.length > 0 && iteration < 5) {
+    endorseSkillsHandler(e, iteration + 1);
+  }
+}
+
+(function() {
+  'use strict';
+  const skillsListContainer = document.querySelector('.pvs-list');
+  
+  const button = document.createElement('button');
+  button.innerText = 'Endorse all';
+  button.style.backgroundColor = '#5783db';
+  button.style.color = '#ffffff';
+  button.style.padding = '20px';
+  button.style.borderRadius = '4px';
+  button.style.position = 'fixed';
+  button.style.zIndex = '99';
+  button.style.bottom = '40px';
+  button.style.left = '40px';
+	
+  button.addEventListener('click', endorseSkillsHandler);
+  document.body.append(button);
+})();
